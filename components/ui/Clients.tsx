35b46a5ef9f5b14df39e5c8788357b5e88f803d5
Hirusha_R <it22283344@my.sliit.lk>
2024-09-05 20:24:23 +0530

    do some code snipets

--- components/ui/Clients.tsx
+++ components/ui/Clients.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { InfiniteMovingCards } from './InfiniteMovingCards'
+import { testimonials } from '@/data'
 
 function Clients() {
   return (
-    <div>Clients</div>
+    <div className="py-20">
+      <h1 className="heading">
+        Responses from the {" "}
+        <span className="text-purple"> Satisfied Clients</span>
+      </h1>
+      <div className="flex flex-col items-center mt-10 ">
+        <InfiniteMovingCards 
+        items={testimonials}
+        direction='right'
+        speed='slow'/>
+      </div>  
+    </div>  
+
   )
 }
 
